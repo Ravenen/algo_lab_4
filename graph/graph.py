d164d65266dc655fc5dc1cbc04aea620b12f0d30
Raven <redstoyningbros@gmail.com>
2020-11-22 21:19:43 +0200

    Added type annotations

--- graph/graph.py
+++ graph/graph.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import DefaultDict, List
+from typing import DefaultDict, List, KeysView
 
 
 class Graph(object):
@@ -7,16 +7,16 @@ class Graph(object):
     def __init__(self):
         self.adjacencies: DefaultDict[str, List[str]] = defaultdict(list)
 
-    def add_edge(self, vertex_from, vertex_to):
+    def add_edge(self, vertex_from: str, vertex_to: str):
         self.adjacencies[vertex_from].append(vertex_to)
 
-    def get_vertices_with_edge(self):
+    def get_vertices_with_edge(self) -> KeysView[str]:
         return self.adjacencies.keys()
 
-    def find_longest_path_from(self, vertex_from):
+    def find_longest_path_from(self, vertex_from: str) -> int:
         return self.dfs(vertex_from, 1)
 
-    def dfs(self, start_node, current_path):
+    def dfs(self, start_node: str, current_path: int) -> int:
         result_path = current_path
         if start_node not in self.adjacencies.keys():
             return result_path
