44371aa10299d3a8f833f05a007c53c88cb5ccca
Raven <redstoyningbros@gmail.com>
2020-11-22 20:31:31 +0200

    Added Graph class support in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import re
-from collections import defaultdict
+
+from graph.graph import Graph
 
 
 def generate_one_letter_differ_regex(string):
@@ -14,29 +15,17 @@ def is_one_letter_differ(full_string, substring):
     return any([regex.search(full_string) for regex in one_letter_differ_regex])
 
 
-def dfs(graph_input, start_node, max_value):
-    result = max_value
-    if start_node not in graph_input.keys():
-        return result
-    for neighbour in graph_input[start_node]:
-        result = max(result, dfs(graph_input, neighbour, max_value + 1))
-    return result
-
-
 if __name__ == '__main__':
-    # str_list = ["b", "bcad", "bca", "bad", "bd", "aaaa", "a", "bbbbbb"]
     str_list = ["crates", "car", "cats", "crate", "rate", "at", "ate", "tea", "rat", "a"]
     str_list = sorted(str_list, key=lambda string: len(string), reverse=True)
-    print(str_list)
-    graph = defaultdict(list)
+    graph = Graph()
 
     for i in range(1, len(str_list)):
         prev_index = i - 1
         while prev_index >= 0 and len(str_list[prev_index]) - len(str_list[i]) <= 1:
             if is_one_letter_differ(str_list[prev_index], str_list[i]):
-                graph[str_list[prev_index]].append(str_list[i])
-                print(str_list[prev_index] + " -> " + str_list[i])
+                graph.add_edge(str_list[prev_index], str_list[i])
             prev_index -= 1
 
-    res = max([dfs(graph, root, 1) for root in graph.keys()])
+    res = max([graph.find_longest_path_from(root) for root in graph.get_vertices_with_edge()])
     print(res)
