ac8db6fec460660ad6c637db3947b8c0a6b5e01b
Raven <redstoyningbros@gmail.com>
2020-11-22 20:45:27 +0200

    Refactored app.py to make code be more clear

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from graph.graph import Graph
 
 def generate_one_letter_differ_regex(string):
     result_regex_list = []
-    for i in range(len(string) + 1):
-        result_regex_list.append(re.compile(str(string[:i] + "." + string[i:])))
+    for dot_position in range(len(string) + 1):
+        result_regex_list.append(re.compile(str(string[:dot_position] + "." + string[dot_position:])))
     return result_regex_list
 
 
@@ -15,17 +15,19 @@ def is_one_letter_differ(full_string, substring):
     return any([regex.search(full_string) for regex in one_letter_differ_regex])
 
 
-if __name__ == '__main__':
-    str_list = ["crates", "car", "cats", "crate", "rate", "at", "ate", "tea", "rat", "a"]
-    str_list = sorted(str_list, key=lambda string: len(string), reverse=True)
-    graph = Graph()
-
-    for i in range(1, len(str_list)):
-        prev_index = i - 1
-        while prev_index >= 0 and len(str_list[prev_index]) - len(str_list[i]) <= 1:
-            if is_one_letter_differ(str_list[prev_index], str_list[i]):
-                graph.add_edge(str_list[prev_index], str_list[i])
+def fill_in_graph_with_words(input_graph, input_words_list):
+    for index in range(1, len(input_words_list)):
+        prev_index = index - 1
+        while prev_index >= 0 and len(input_words_list[prev_index]) - len(input_words_list[index]) <= 1:
+            if is_one_letter_differ(input_words_list[prev_index], input_words_list[index]):
+                input_graph.add_edge(input_words_list[prev_index], input_words_list[index])
             prev_index -= 1
 
+
+if __name__ == '__main__':
+    words_list = ["crates", "car", "cats", "crate", "rate", "at", "ate", "tea", "rat", "a"]
+    words_list = sorted(words_list, key=lambda string: len(string), reverse=True)
+    graph = Graph()
+    fill_in_graph_with_words(graph, words_list)
     res = max([graph.find_longest_path_from(root) for root in graph.get_vertices_with_edge()])
     print(res)
